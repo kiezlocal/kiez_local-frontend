eabcbf477f53f82a85223ff70597682de9965591
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-05-29 22:38:19 +0200

    Add pages and components skeletons, apply routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import {Route, Routes} from "react-router-dom";
+import "/App.css";
+import Kiez from "./pages/Kiez";
+import KiezDetails from "./pages/KiezDetails";
+import EventDetails from "./pages/EventDetails"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Header/>
+
+<Routes>
+      <Route path="/"> </Route>
+      <Route path="/kiezs" element={<Kiez/>}></Route>
+      <Route path="/kiezs/:kiezId" element={<KiezDetails/>}></Route>
+      <Route path="/events/:eventId" element={<EventDetails/>}></Route>
+      <Route path="/about" element= {<About/>}></Route>
+      <Route path="*" element= {<NotFound/>}></Route>
+      
+</Routes>
+
+      <Footer/>
+</>
+  );
 }
 
 export default App
