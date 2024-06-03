fda30d5879ba3f2293ee841f2835ee522763816e
Katrin Winnen <katrin.riske@icloud.com>
2024-06-03 09:39:47 +0200

    added Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ function App() {
 
   return (
     <>
-      <Navbar/>
 <Router>
+<Navbar/>
 <div>
 <Routes>
       <Route path="/" element={<Homepage />}/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
-import {useEffect, useState} from 'react';
-import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-left">
+                <Link to="/">
+                    <img src="https://via.placeholder.com/150" alt="Logo" className="logo" />
+                </Link>
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/kiez" className="nav-link">Kiez</Link>
+                <Link to="/about" className="nav-link">About</Link>
+            </div>
+            <div className="navbar-right">
+                <Link to="/login" className="nav-link">Login</Link>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
