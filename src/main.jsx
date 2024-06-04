f4b4dbd11d465262fcdd17a5cd9cf60374f56c44
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-06-05 01:54:49 +0200

    Styling kiez grid

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthProviderWrapper } from '../context/auth.context.jsx'
+import { ChakraProvider } from '@chakra-ui/react';
+
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -12,8 +14,10 @@ const rootElement = document.getElementById('root')
 ReactDOM.createRoot(rootElement).render(
 
   <React.StrictMode>
+    <ChakraProvider>
     <AuthProviderWrapper>
     <App />
     </AuthProviderWrapper>
+    </ChakraProvider>
   </React.StrictMode>,
 )
--- src/pages/Kiez.jsx
+++ src/pages/Kiez.jsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom'; 
 import "../App.css";
-import {Card, CardBody, Heading, Image, SimpleGrid, Stack } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, SimpleGrid, Stack, Text } from "@chakra-ui/react"; 
 
 
 
