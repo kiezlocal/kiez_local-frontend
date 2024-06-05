38c2f447e6befcdb18066247ec24042ad1fb3ef8
Katrin Winnen <katrin.riske@icloud.com>
2024-06-05 12:49:18 +0200

    added authentication

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const SignupPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+  const handleUsernameChange = (e) => setUsername(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { email, password, name: username };
+
+    axios.post(`${API_URL}/auth/signup`, requestBody)
+      .then((response) => {
+        navigate("/login");
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+
+  return (
+    <div className="SignupPage">
+      <h1>Sign Up</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>Email:</label>
+        <input type="email" name="email" value={email} onChange={handleEmailChange} />
+
+        <label>Password:</label>
+        <input type="password" name="password" value={password} onChange={handlePasswordChange} />
+
+        <label>Username:</label>
+        <input type="text" name="username" value={username} onChange={handleUsernameChange} />
+
+        <button type="submit">Sign Up</button>
+      </form>
+
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+      <p>Already have an account?</p>
+      <Link to="/login">Login</Link>
+    </div>
+  );
+};
+
+export default SignupPage;
