637771e89bbd8421bc27f25ce67f7a7ad4f47b11
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-06-06 09:33:54 +0200

    signup styling

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,18 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import {
+  Stack,
+  Flex,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Checkbox,
+  Image,
+  Text,
+} from '@chakra-ui/react';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -30,28 +42,71 @@ const SignupPage = () => {
   };
 
   return (
-    <div className="SignupPage">
-      <h1>Sign Up</h1>
+    // <div className="SignupPage">
+    //   <h1>Sign Up</h1>
 
-      <form onSubmit={handleSubmit}>
-        <label>Email:</label>
-        <input type="email" name="email" value={email} onChange={handleEmailChange} />
+    //   <form onSubmit={handleSubmit}>
+    //     <label>Email:</label>
+    //     <input type="email" name="email" value={email} onChange={handleEmailChange} />
 
-        <label>Password:</label>
-        <input type="password" name="password" value={password} onChange={handlePasswordChange} />
+    //     <label>Password:</label>
+    //     <input type="password" name="password" value={password} onChange={handlePasswordChange} />
 
-        <label>Username:</label>
-        <input type="text" name="username" value={username} onChange={handleUsernameChange} />
+    //     <label>Username:</label>
+    //     <input type="text" name="username" value={username} onChange={handleUsernameChange} />
 
-        <button type="submit">Sign Up</button>
-      </form>
+    //     <button type="submit">Sign Up</button>
+    //   </form>
 
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
+    //   {errorMessage && <p className="error-message">{errorMessage}</p>}
 
-      <p>Already have an account?</p>
-      <Link to="/login">Login</Link>
-    </div>
-  );
+    //   <p>Already have an account?</p>
+    //   <Link to="/login">Login</Link>
+    // </div>
+
+
+<Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+      <Flex p={8} flex={1} align={'center'} justify={'center'}>
+        <Stack spacing={4} w={'full'} maxW={'md'}>
+          <Heading fontSize={'2xl'}>Register</Heading>
+          <form onSubmit={handleSubmit}>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" name="email" value={email} onChange={handleEmailChange} />
+            </FormControl>
+            <FormControl id="password" isRequired mt={4}>
+              <FormLabel>Password</FormLabel>
+              <Input type="password" name="password" value={password} onChange={handlePasswordChange} />
+              </FormControl>
+              <FormControl id="username" isRequired mt={4}>
+              <FormLabel>Username</FormLabel>
+              <Input type="username" name="username" value={username} onChange={handleUsernameChange} />
+            </FormControl>
+            {errorMessage && <Text color="red.500" mt={2}>{errorMessage}</Text>}
+            <Stack spacing={6} mt={5}>
+              
+              <Button type="submit" colorScheme={'blue'} variant={'solid'}>
+                Sign up
+              </Button>
+            </Stack>
+          </form>
+          <Text mt={4}>Already have an account?<Link color={'blue.500'} to={"/signup"}>Login</Link></Text>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={'Login Image'}
+          objectFit={'cover'}
+          src={
+            'https://berlinstreetart.com/wp-content/uploads/2016/03/Klunkerkranich-Berlin-via-Facebook.jpg'
+          }
+        />
+      </Flex>
+    </Stack>
+
+        )
+
+
 };
 
 export default SignupPage;
